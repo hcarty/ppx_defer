1af1317a7826e4476ba36fc7c5f9fecfc021a457
Hezekiah M. Carty <hez@0ok.org>
2020-05-05 00:47:15 -0600

    Get tests working on Windows (#5)

--- examples/ic.ml
+++ examples/ic.ml
@@ -1,6 +1,7 @@
 let () =
+  set_binary_mode_out stdout true;
   let ic = open_in_bin "ic.ml" in
   [%defer close_in ic];
   let length = in_channel_length ic in
   let bytes = really_input_string ic length in
-  print_endline bytes
+  print_string bytes
